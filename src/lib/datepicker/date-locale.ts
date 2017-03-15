e634ddeeacb6b75c0b12422cab7764ea95d84fe8
Łukasz Mozyrski <lukasz.mozyrski@gmail.com>
2017-03-15 08:44:35 +0100

    Update date-locale.ts

--- src/lib/datepicker/date-locale.ts
+++ src/lib/datepicker/date-locale.ts
@@ -8,28 +8,28 @@ export class DateLocale {
   firstDayOfWeek = 0;
 
   months = [
-    { full: 'January', short: 'Jan' },
-    { full: 'February', short: 'Feb' },
-    { full: 'March', short: 'Mar' },
-    { full: 'April', short: 'Apr' },
-    { full: 'May', short: 'May' },
-    { full: 'June', short: 'Jun' },
-    { full: 'July', short: 'Jul' },
-    { full: 'August', short: 'Aug' },
-    { full: 'September', short: 'Sep' },
-    { full: 'October', short: 'Oct' },
-    { full: 'November', short: 'Nov' },
-    { full: 'December', short: 'Dec' },
+    { full: 'Styczeń', short: 'STY' },
+    { full: 'Luty', short: 'LUT' },
+    { full: 'Marzec', short: 'MAR' },
+    { full: 'Kwiecień', short: 'KWI' },
+    { full: 'MAJ', short: 'MAJ' },
+    { full: 'Czerwiec', short: 'CZE' },
+    { full: 'Lipiec', short: 'LIP' },
+    { full: 'Sierpień', short: 'SIE' },
+    { full: 'Wrzesień', short: 'WRZ' },
+    { full: 'Październik', short: 'PAŹ' },
+    { full: 'Listopad', short: 'LIS' },
+    { full: 'Grudzień', short: 'GRU' },
   ];
 
   days = [
-    { full: 'Sunday', short: 'Sun', xshort: 'S' },
-    { full: 'Monday', short: 'Mon', xshort: 'M' },
-    { full: 'Tuesday', short: 'Tue', xshort: 'T' },
-    { full: 'Wednesday', short: 'Wed', xshort: 'W' },
-    { full: 'Thursday', short: 'Thu', xshort: 'T' },
-    { full: 'Friday', short: 'Fri', xshort: 'F' },
-    { full: 'Saturday', short: 'Sat', xshort: 'S' },
+    { full: 'Niedziela', short: 'Niedz.', xshort: 'N' },
+    { full: 'Poniedziałek', short: 'Pon.', xshort: 'Pn' },
+    { full: 'Wtorek', short: 'Wt.', xshort: 'W' },
+    { full: 'Środa', short: 'Śr.', xshort: 'Ś' },
+    { full: 'Czwartek', short: 'Czw.', xshort: 'Cz' },
+    { full: 'Piątek', short: 'Pt.', xshort: 'Pt' },
+    { full: 'Soboa', short: 'Sob.', xshort: 'S' },
   ];
 
   getDateLabel(d: number) { return `${d}`; }
